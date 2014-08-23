8954f51bab076d5a74c686995f35618100fded41
oatsey <mikehadam@gmail.com>
2014-08-23 09:54:31 -0400

    adding color support

--- langtons_ant.cpp
+++ langtons_ant.cpp
@@ -10,9 +10,17 @@ struct Ant {
 	int y = 0;
 };
 
-int checkAngle();
+void HSVtoRGB(int, int, int);
+
+void decodeOneStep(const char*, vector<unsigned char>&, unsigned int&, unsigned int&);
 void encodeOneStep(const char*, vector<unsigned char>&, int, int);
-int iterateAnt(vector<unsigned char>&, Ant&);
+
+int generateColor();
+
+int workAnt(vector<unsigned char>&, int, int, Ant&);
+int moveAnt(Ant&);
+int trapAnt(Ant&, int, int);
+
 int createBox(int, int, int, int, int, int, vector<unsigned char>&);
 
 int main() {
@@ -21,16 +29,34 @@ int main() {
 	
 	// create image
 	vector<unsigned char> image;
-	int height = 1200; int width = 1920;
+	unsigned int height = 0; unsigned int width = 0; // image properties
+	
+	decodeOneStep("test1.png", image, width, height);
+	cout << "Decoded!" << endl; // success loading image
+	cout << "Width: " << width << endl;
+	cout << "Height: " << height << endl;
+	
 	image.resize(height * width * 4);
 	
 	ant.x = width / 2 - 1;
 	ant.y = height / 2 - 1;
 	cout << ant.x << " " << ant.y << endl;
 	
-	iterateAnt(image, ant);
-	createBox(0, width/2-1, 0, height/2-1, 1920, 1080, image);
-	createBox(width/2-1, width, height/2-1, height, 1920, 1080, image);
+	//createBox(0, width, 0, height, 1920, 1200, image);
+	
+	vector<int> colors = {};
+	
+	colors.append(rand());
+	
+	for (long long i = 0; i < 100000000; i++) {
+		workAnt(image, width, height, ant);
+		moveAnt(ant);
+		trapAnt(ant, width, height);
+		colors.append() = generateColor(colors.end());
+	}
+	
+	//createBox(0, width/2-1, 0, height/2-1, 1920, 1080, image);
+	//createBox(width/2-1, width, height/2-1, height, 1920, 1080, image);
 	
 	/*for (int y = 0; y < height; y++)
 	for (int x = 0; x < width; x++) {
@@ -65,6 +91,17 @@ int main() {
 	return 0;
 }
 
+void decodeOneStep(const char* filename, vector<unsigned char>& image, unsigned int& width, unsigned int& height) {
+
+  //decode
+  unsigned error = lodepng::decode(image, width, height, filename);
+
+  //if there's an error, display it
+  if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+
+  //the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
+}
+
 void encodeOneStep(const char* filename, std::vector<unsigned char>& image, int width, int height) {
 	//Encode the image
 	unsigned error = lodepng::encode(filename, image, width, height);
@@ -73,12 +110,63 @@ void encodeOneStep(const char* filename, std::vector<unsigned char>& image, int 
 	if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
 }
 
-int checkAngle() {
-
+int trapAnt(Ant& ant, int x_lim, int y_lim) {
+	if (ant.x >= x_lim)
+		ant.x = x_lim - 1;
+	else if (ant.x < 0)
+		ant.x = 0;
+	if (ant.y >= y_lim)
+		ant.y = y_lim - 1;
+	else if (ant.y < 0)
+		ant.y = 0;
+		
 	return 0;
 }
 
-int iterateAnt(vector<unsigned char>& image, Ant& ant) {
+int moveAnt(Ant& ant) {
+	switch (ant.vector) {
+	case 0:
+		ant.x += 1;
+		break;
+	case 90:
+		ant.y += 1;
+		break;
+	case 180:
+		ant.x -= 1;
+		break;
+	case 270:
+		ant.y -= 1;
+		break;
+	}
+	
+	return 0;
+}
+
+int workAnt(vector<unsigned char>& image, int width, int height, Ant& ant) {
+	int x = ant.x;
+	int y = ant.y;
+	unsigned char image_a = image[4 * width * y + 4 * x + 3]; // alpha
+	unsigned char image_r = image[4 * width * y + 4 * x + 0]; // red
+	unsigned char image_g = image[4 * width * y + 4 * x + 1]; // green
+	unsigned char image_b = image[4 * width * y + 4 * x + 2]; // blue
+	if (((image_r + image_g + image_b) / 3) < 128) {
+		image[4 * width * y + 4 * x + 0] ^= 0xFF;
+    	image[4 * width * y + 4 * x + 1] ^= 0xFF;
+    	image[4 * width * y + 4 * x + 2] ^= 0xFF;
+    	if ((ant.vector + 90) < 360)
+    		ant.vector += 90;
+    	else
+    		ant.vector = 0;
+	} else {
+		image[4 * width * y + 4 * x + 0] ^= 0xAA;
+    	image[4 * width * y + 4 * x + 1] ^= 0xAA;
+    	image[4 * width * y + 4 * x + 2] ^= 0xAA;
+    	if ((ant.vector - 90) > 0)
+    		ant.vector -= 90;
+    	else
+    		ant.vector = 270;
+	};
+	
 	
 	return 0;
 }
@@ -86,13 +174,32 @@ int iterateAnt(vector<unsigned char>& image, Ant& ant) {
 int createBox(int x1, int x2, int y1, int y2, int width, int height, vector<unsigned char>& image) {
 	for (int x = x1; x < x2; x++){
 	for (int y = y1; y < y2; y++) {
-		if (true) {
-			image[4 * width * y + 4 * x + 0] = 100;
-    		image[4 * width * y + 4 * x + 1] = 50;
-    		image[4 * width * y + 4 * x + 2] = 50;
-    		image[4 * width * y + 4 * x + 3] = 255;
-    	}
+		image[4 * width * y + 4 * x + 0] = 0;
+    	image[4 * width * y + 4 * x + 1] = 0;
+    	image[4 * width * y + 4 * x + 2] = 0;
+    	image[4 * width * y + 4 * x + 3] = 255;
 	}
 	}
 	return 0;
+}
+
+void HSVtoRGB(unsigned char &R, unsigned char &G, unsigned char &B, int H, unsigned char S, unsigned char V) {
+	unsigned char min, max, delta;
+	
+	if (R < G) min = R else min = g;ls
+	
+	if (
+}
+
+int generateColors(int previous) {
+	int inversePhi = 0.618033988749895;
+	
+	previous += inversePhi;
+	previous %= 1;
+	
+	int R, G, B;
+	
+	HSVtoRGB(R, G, B, previous, 0.5, 0.95);
+	
+	return
 }
